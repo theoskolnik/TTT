a14748be1f7a2d4755dc00e3fc721a0e400698df
Theo Skolnik <theodoraskolnik@gmail.com>
2015-01-17 09:31:08 -0600

    re-adds iml file

--- test/BoardTest.java
+++ test/BoardTest.java
@@ -7,8 +7,6 @@ import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 public class BoardTest {
     private PrintStream printStream;
